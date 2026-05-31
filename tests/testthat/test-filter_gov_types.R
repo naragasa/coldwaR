688cbe34b9ccfcd847f28fcccf0c6e24b27732c4
Ian Cay <icay@calpoly.edu>
2026-05-30 23:02:21 -0700

    Tests for filtering helper functions

---
+++ tests/testthat/test-filter_gov_types.R
@@ -0,0 +1,26 @@
+test_that("filter_gov_types works", {
+
+  result_default <- filter_gov_types()
+
+  result_single <- filter_gov_types("Pure Democracy")
+
+  result_vector <- filter_gov_types(c("Pure Democracy", "Communist"))
+
+  expect_s3_class(result_default, "data.frame")
+  expect_s3_class(result_single,  "data.frame")
+  expect_s3_class(result_vector,  "data.frame")
+
+  expect_equal(
+    sort(unique(result_default$gov_type)),
+    sort(c("Pure Democracy", "Pure Monarchy", "Democratic Monarchy", "Communist", "Other"))
+  )
+
+  expect_true(all(result_single$gov_type == "Pure Democracy"))
+
+  expect_true(all(result_vector$gov_type %in% c("Pure Democracy", "Communist")))
+
+  expect_error(
+    filter_gov_types("Invalid Type"),
+    "Selected types must be in:"
+  )
+})
---
+++ tests/testthat/test-filter_regimes.R
@@ -0,0 +1,27 @@
+test_that("filter_regimes works", {
+
+  result_default <- filter_regimes()
+
+  result_single <- filter_regimes("Parliamentary democracy")
+
+  result_vector <- filter_regimes(c("Parliamentary democracy", "Military dictatorship"))
+
+  expect_s3_class(result_default, "data.frame")
+  expect_s3_class(result_single,  "data.frame")
+  expect_s3_class(result_vector,  "data.frame")
+
+  expect_equal(
+    sort(unique(result_default$regime_category)),
+    sort(c("Parliamentary democracy", "Civilian dictatorship", "Presidential democracy",
+           "Military dictatorship", "Mixed democratic", "Royal dictatorship"))
+  )
+
+  expect_true(all(result_single$regime_category == "Parliamentary democracy"))
+
+  expect_true(all(result_vector$regime_category %in% c("Parliamentary democracy", "Military dictatorship")))
+
+  expect_error(
+    filter_regimes("Invalid Regime"),
+    "Selected types must be in:"
+  )
+})
